1f74c0e0b8532a596e723a4468659d2e2f5cde18
Ruwanima <pasinduruwanima1002@gmail.com>
2025-10-31 19:25:27 +0530

    create project service model

---
+++ Service_Management_Service/Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Service_Management_Service.Models
+{
+    public class Project
+    {
+        [Key]
+        public Guid ProjectId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(100)]
+        public string ProjectTitle { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string? ProjectDescription { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Status { get; set; } = "Requested";
+        // Possible values: Requested, InProgress, Completed, Cancelled
+
+        [Required]
+        public decimal EstimatedVAT { get; set; } // VAT or Tax applied to the project
+
+        [DataType(DataType.DateTime)]
+        public DateTime RequestDate { get; set; } = DateTime.UtcNow;
+
+        [DataType(DataType.DateTime)]
+        public DateTime? CompletedDate { get; set; }
+    }
+}
