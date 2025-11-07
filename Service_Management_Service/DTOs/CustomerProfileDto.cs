2af135a20fd0d72e9cc3b2ac385bfe2ecc25c5d6
Ruwanima <pasinduruwanima1002@gmail.com>
2025-11-07 11:22:27 +0530

    add to customer profile endpoints

---
+++ Service_Management_Service/DTOs/CustomerProfileDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Service_Management_Service.DTOs
+{
+    public class CustomerDTO
+    {
+        public string UserID { get; set; } = null!;
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; }
+        public int LoyaltyPoints { get; set; }
+        public string? Address { get; set; }
+    }
+
+    public class UpdateCustomerDTO
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int LoyaltyPoints { get; set; }
+        public string? Address { get; set; }
+    }
+}
